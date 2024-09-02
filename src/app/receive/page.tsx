7c9daaa4a85b9bb26cf6c1df38769dba31ab85ae
Victor Okpukpan <vokpukpan@gmail.com>
2024-09-02 13:03:29 +0100

    add button functionality

--- src/app/receive/page.tsx
+++ src/app/receive/page.tsx
@@ -1,13 +1,49 @@
+"use client";
+
+import { useState } from "react";
 import Image from "next/image";
 import { IoShareOutline } from "react-icons/io5";
 import { MdOutlineContentCopy } from "react-icons/md";
+import { FaX } from "react-icons/fa6";
+import { useRouter } from "next/navigation";
 
 export default function Receive() {
+  const router = useRouter();
+  const [showNotification, setShowNotification] = useState(false);
+
+  const handleCopy = () => {
+    const textToCopy = "UQAaihBgkQvEngiOT1eiGAJ10 31OE3g1EE2KxarCMjCMNx3R";
+    navigator.clipboard.writeText(textToCopy).then(
+      () => {
+        setShowNotification(true);
+        setTimeout(() => {
+          setShowNotification(false);
+        }, 2000);
+      },
+      (err) => {
+        console.error("Failed to copy text: ", err);
+      }
+    );
+  };
+
   return (
     <main>
-      <div className="min-h-screen px-4 py-9">
+      <div className="min-h-screen px-4 py-9 relative">
+        {showNotification && (
+          <div className="absolute top-10 left-1/2 transform -translate-x-1/2 bg-gray-600 text-white text-center py-2 px-4 rounded-[100px] mt-4">
+            Text copied to clipboard!
+          </div>
+        )}
+        <div className="flex justify-end">
+          <button
+            onClick={() => router.push("/")}
+            className="bg-[#202633] text-white rounded-full p-3"
+          >
+            <FaX />
+          </button>
+        </div>
         <div className="text-center">
-          <h1 className="text-white font-bold text-2xl">Recieve Toncoin</h1>
+          <h1 className="text-white font-bold text-2xl">Receive Toncoin</h1>
           <p className="text-[#FFFFFF80] font-medium text-lg mt-2">
             Send only Toncoin TON and tokens in
             <br />
@@ -31,7 +67,10 @@ export default function Receive() {
         </div>
 
         <div className="mt-5 flex items-center space-x-3 w-max mx-auto">
-          <button className="text-white py-4 px-6 flex items-center space-x-1 bg-[#FFFFFF4A] rounded-[100px] text-base font-medium">
+          <button
+            onClick={handleCopy}
+            className="text-white py-4 px-6 flex items-center space-x-1 bg-[#FFFFFF4A] rounded-[100px] text-base font-medium"
+          >
             <MdOutlineContentCopy className="w-[20px] h-[20px] mb-2" />
             <span>Copy</span>
           </button>
--- src/app/send/page.tsx
+++ src/app/send/page.tsx
@@ -1,40 +1,120 @@
 "use client";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { FaX } from "react-icons/fa6";
+import { MdOutlineKeyboardArrowLeft } from "react-icons/md";
 
 export default function Send() {
+  const router = useRouter();
+
   const [address, setAddress] = useState("");
   const [comment, setComment] = useState("");
+
+  const [page, setPage] = useState("address");
+
+  const handlePaste = async () => {
+    try {
+      const text = await navigator.clipboard.readText();
+      setAddress(text);
+    } catch (err) {
+      console.error("Failed to read clipboard contents: ", err);
+    }
+  };
+
+  const handleBackToHome = () => {
+    router.push("/"); // Navigate to the home page
+  };
 
   return (
     <main>
       <div className="h-screen px-4 py-9">
-        <form action="" className="flex flex-col h-full">
-          <h1 className="text-2xl font-bold text-white mb-6 text-center">
-            Recipient
-          </h1>
-          <div className="">
-            <input
-              type="text"
-              value={address}
-              onChange={(e) => setAddress(e.target.value)}
-              placeholder="Address or name"
-              className="rounded-[20px] py-6 px-5 bg-[#202633] text-white font-medium text-xl w-full"
-            />
-            <input
-              type="text"
-              value={comment}
-              onChange={(e) => setComment(e.target.value)}
-              placeholder="Comment"
-              className="rounded-[20px] py-6 px-5 mt-6 bg-[#202633] text-white font-medium text-xl w-full"
-            />
-          </div>
-          <button
-            disabled={!address}
-            className="bg-[#399DD1] w-full py-6 font-medium text-lg text-white disabled:cursor-not-allowed disabled:text-[#FFFFFF80] rounded-[20px] mt-auto"
-          >
-            Continue
-          </button>
-        </form>
+        {page === "address" && (
+          <form action="" className="flex flex-col h-full">
+            <div className="flex items-center justify-between mb-6">
+              <div className=""></div>
+              <h1 className="text-2xl font-bold text-white text-center">
+                Recipient
+              </h1>
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  router.push("/");
+                }}
+                className="bg-[#202633] text-white rounded-full p-3"
+              >
+                <FaX />
+              </button>
+            </div>
+            <div className="w-full">
+              <div className="relative w-full">
+                <input
+                  type="text"
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                  placeholder="Address or name"
+                  className="rounded-[20px] outline-[#399DD1] border-0 py-6 px-5 bg-[#202633] text-white font-medium text-xl w-full"
+                />
+                {address.length === 0 && (
+                  <button
+                    type="button"
+                    onClick={handlePaste}
+                    className="absolute right-6 top-6 text-[#399DD1] font-medium text-lg"
+                  >
+                    Paste
+                  </button>
+                )}
+              </div>
+              <input
+                type="text"
+                value={comment}
+                onChange={(e) => setComment(e.target.value)}
+                placeholder="Comment"
+                className="rounded-[20px] outline-[#399DD1] py-6 px-5 mt-6 bg-[#202633] text-white font-medium text-xl w-full"
+              />
+            </div>
+            <button
+              disabled={!address}
+              onClick={() => setPage("transfer")}
+              className="bg-[#399DD1] w-full py-6 font-medium text-lg text-white disabled:cursor-not-allowed disabled:text-[#FFFFFF80] rounded-[20px] mt-auto"
+            >
+              Continue
+            </button>
+          </form>
+        )}
+
+        {page === "transfer" && (
+          <form action="">
+            <div className="flex justify-between items-center">
+              <button
+                onClick={() => setPage("address")}
+                className="bg-[#202633] text-white rounded-full p-3"
+              >
+                <MdOutlineKeyboardArrowLeft />
+              </button>
+              <div className="">
+                <h1 className="text-2xl font-bold text-white mb-1 text-center">
+                  Amount
+                </h1>
+                <p className="text-[#FFFFFF80] text-lg text-center font-medium">
+                  To: {address}
+                </p>
+              </div>
+              <button
+                onClick={(e) => {
+                  e.preventDefault();
+                  router.push("/");
+                }}
+                className="bg-[#202633] text-white rounded-full p-3"
+              >
+                <FaX />
+              </button>
+            </div>
+
+            <div className="p-6 bg-[#202633] rounded-[20px]">
+                
+            </div>
+          </form>
+        )}
       </div>
     </main>
   );
